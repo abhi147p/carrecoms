80b9b9be93d0eee0c3efb2405622aa2f3064918c
abhi147p <abhishekpuppala60@gmail.com>
2024-12-01 23:56:53 -0600

    App correction

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import openai
 
 app = Flask(__name__)
 
-# Set your OpenAI API key
-openai.api_key = OPEN_AI_KEY
+
+from openai import OpenAI
+client = OpenAI(
+  api_key = OPEN_AI_KEY
+)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -15,17 +18,19 @@ def webhook():
     user_query = req['queryResult']['queryText']
 
     # Send user query to GPT API
-    response = openai.ChatCompletion.create(
-        model="gpt-4",  # Choose your GPT model
+    completion = client.chat.completions.create(
+        model="gpt-4o",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": user_query}
+            {"role": "system", "content": "You are a helpful assistant for answering all questions, mainly questions about cars."},
+            {
+                "role": "user",
+                "content": user_query
+            }
         ]
     )
-
-    # Extract GPT response
-    gpt_response = response['choices'][0]['message']['content'].strip()
-
+    
+    gpt_response = completion.choices[0].message.content.strip()
+    
     # Return GPT response to Dialogflow
     return jsonify({
         "fulfillmentMessages": [
